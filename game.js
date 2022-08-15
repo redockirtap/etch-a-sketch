16c2649217621d3ddfa70001c02ba3ce63112757
Partrickoder <redockirtap@gmail.com>
2022-08-15 21:08:08 +0300

    Finish multicolor version. Set limit for user input to 100.

--- game.js
+++ game.js
@@ -14,55 +14,65 @@ toggleBtn.addEventListener('click', showNavBar);
 
 // ETCH A SKETCH CODE
 // GRID DRAWING AND SIZING
-const gridContainer = document.querySelector('.grid');
 const gridBtn = document.querySelector('#grid-size');
 
-function removeOldGrid() {
+function removeOldGrid(gridContainer) {
     while (gridContainer.firstChild) {
         gridContainer.removeChild(gridContainer.firstChild);
     }
 }
 
-gridCell = function createGridCell() {
+
+gridCell = function createGridCell(gridContainer) {
     const cell = document.createElement('div');
     cell.classList.add('grid-cell');
     gridContainer.append(cell);
 };
 
-drawGrid = function createGrid(gridSize = 16) {
-    removeOldGrid();
-    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-    gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-    
+function gridIterator(gridSize, gridContainer) {
     let i = gridSize*gridSize;
     while (i) {
-        gridCell();
+        gridCell(gridContainer);
         i--;
     }
+}
+
+
+drawGridWithColors = function createGrid(gridSize = 16) {
+    const gridContainer = document.querySelector('.grid'); // Defining grid container
+    removeOldGrid(gridContainer); // removing previous grid container, if such exist
+
+
+    // Drawing the new grid container
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    gridIterator(gridSize, gridContainer);
+
+    // Defining a NodeList of grid cells and adding an Event Listener
+    const uncoloredCellsNodeList = document.querySelectorAll('.grid-cell');
+    uncoloredCellsNodeList.forEach(e =>
+     e.addEventListener('mouseover', getCellIndex.bind(e, uncoloredCellsNodeList)));
 };
 
 getSizeAndCreateGrid = function userGridSize() {
-    gridSize = parseInt(prompt('Write down the size of the grid.'));
-    drawGrid(gridSize);
+    // User inputs a value of a grid size, which PASSES to drawGridWithColors()
+    let gridSize;
+    gridSize = parseInt(prompt("Write down the size of the grid. Warning: 100 is maximum!"));
+    if (gridSize > 100) {
+        gridSize = 100;
+    } else if (isNaN(gridSize)) {
+        gridSize = 16;
+    }
+    console.log(gridSize);
+    drawGridWithColors(gridSize);
 }
 
 gridBtn.addEventListener('click', getSizeAndCreateGrid);
 
-drawGrid();
+drawGridWithColors();
 
 
 // COLORING THE GRID
-
-const uncoloredCellsNodeList = document.querySelectorAll('.grid-cell');
-
-
-function updateCellsNodeList() {
-
-    uncoloredCellsNodeList = document.querySelectorAll('.grid-cell');
-    return uncoloredCellsNodeList;
-}
-
-console.log(uncoloredCellsNodeList);
 
 function random() { 
     maxNum = Math.ceil(254);
@@ -70,28 +80,21 @@ function random() {
     return Math.floor(Math.random() * (maxNum - minNum + 1) + minNum);
 }
 
-cellIndex = function getCellIndex() {
-    // updateCellsNodeList();
+function getCellIndex(uncoloredCellsNodeList) {
+    // Creating an array from Nodelist and receiving its items' indexes
     const cellsArray = Array.from(uncoloredCellsNodeList);
-    // console.log(cellsArray);
-    // console.log(this);
-    // console.log(cellsArray.indexOf(this));
     coloredCellIndex = cellsArray.indexOf(this);
-    addColorCell(coloredCellIndex);
+    // PASS Items' indexes and the Node List to addColorCell()
+    addColorCell(coloredCellIndex, uncoloredCellsNodeList);
 }
 
-
-
-function addColorCell() {
-    // console.log(coloredCellIndex);
+function addColorCell(coloredCellIndex, uncoloredCellsNodeList) {
     randomRGB1 = random();
     randomRGB2 = random();
     randomRGB3 = random();
     uncoloredCellsNodeList[coloredCellIndex].style.backgroundColor = `rgb(${randomRGB1}, ${randomRGB2}, ${randomRGB3})`;
-    // console.log(`hi`);
 }
 
 
-uncoloredCellsNodeList.forEach(eventCell => eventCell.addEventListener('mouseover', cellIndex));
 
 
